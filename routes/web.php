e413645992969369d1a1bf99a38003edbf4bf770
nepriantodeveloper <neprianto@gmail.com>
2023-09-03 18:06:12 +0700

    opsi template ke 2

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ Route::get('/', function () {
     return view('front.index');
 });
 
+// Route::get('/', function () {
+//     return view('front.index2');
+// });
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
